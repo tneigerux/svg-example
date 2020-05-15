77b21dec96f64b9ae5d452024248dff909809212
tneiger <ted@iopendesign.com>
2020-05-15 09:32:52 -0400

    Addressed remaining issues with email modal pop-up.

--- src/site/_includes/js/hello.js
+++ src/site/_includes/js/hello.js
@@ -1,4 +1,13 @@
+// Open the "Receive email updates" address collector.
+
 clickHandler('.sidebar .actions h3', function(){
   let hiddenForm = document.getElementsByClassName('email-modal');
   hiddenForm[0].style.display = "block";
+});
+
+// Close the address collector with no entry.
+
+clickHandler('.close-me', function(){
+  let hiddenForm = document.getElementsByClassName('email-modal');
+  hiddenForm[0].style.display = "none";
 });
