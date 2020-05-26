e381ecfeb3e62f2ad6afe3fa806db51fa949a410
tneiger <ted@iopendesign.com>
2020-05-25 22:59:12 -0400

    Added dismissal via ESC key to email collector.

--- src/site/_includes/js/clicks.js
+++ src/site/_includes/js/clicks.js
@@ -1,13 +1,21 @@
 // Open the "Receive email updates" address collector.
 
-clickHandler('.sidebar .actions h3', function(){
+eventHandler('.sidebar .actions h3', 'click', function(){
   let hiddenForm = document.getElementsByClassName('email-modal');
   hiddenForm[0].style.display = "block";
 });
 
 // Close the address collector with no entry.
 
-clickHandler('.close-me', function(){
+eventHandler('.close-me', 'click', function(){
   let hiddenForm = document.getElementsByClassName('email-modal');
   hiddenForm[0].style.display = "none";
+});
+
+// Close the address collector with no entry via ESC key
+
+eventHandler('document', 'keydown', function(){
+  let hiddenForm = document.getElementsByClassName('email-modal')
+  if (event.which == 27) 
+    hiddenForm[0].style.display = "none";
 });
