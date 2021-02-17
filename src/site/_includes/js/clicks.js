c029dcaf91a57075df99c9f6551711bad712e62f
alexx <alexxc46@gmail.com>
2021-02-17 06:53:36 +0200

    contact form changed to backendless

--- src/site/_includes/js/clicks.js
+++ src/site/_includes/js/clicks.js
@@ -1,20 +1,20 @@
 // Open the "Receive email updates" address collector.
 
-eventHandler('.sidebar .actions h3', 'click', function(){
+eventHandler('.sidebar .actions h3', 'click', function () {
   let hiddenForm = document.getElementsByClassName('email-modal');
   hiddenForm[0].style.display = "block";
 });
 
 // Close the address collector with no entry.
 
-eventHandler('.cancel-submit-btn', 'click', function(){
+eventHandler('.cancel-submit-btn', 'click', function () {
   let hiddenForm = document.getElementsByClassName('email-modal');
   hiddenForm[0].style.display = "none";
 });
 
 // Activate hamburger menu
 
-eventHandler('.header__burger', 'click', function(){
+eventHandler('.header__burger', 'click', function () {
   document.querySelector('.header__burger').classList.toggle('active');
   document.querySelector('.header__menu').classList.toggle('active');
   document.querySelector('body').classList.toggle('lock');
@@ -23,7 +23,7 @@ eventHandler('.header__burger', 'click', function(){
 
 // Show-hide contact-form
 
-eventHandler('.expand-form', 'click', function(){
+eventHandler('.expand-form', 'click', function () {
   document.querySelector('#expand-form').classList.add('form-invisible');
   document.querySelector('.contact-form').classList.remove('form-invisible');
   //IE9 support?
@@ -52,19 +52,19 @@ Array.from(homeOptionsTabs).forEach((item, index) => {
 
     homeOptionsTabs[index].classList.add('active'); /* active for tab */
     homeOptions[index].classList.add('active'); /* active for option section */
-  } );
+  });
 });
 
 // mobile - show-hide home page options
 var optionsItems = document.getElementsByClassName("options-box-mob__option-tab");
-Array.from(optionsItems).forEach(function(item) {
+Array.from(optionsItems).forEach(function (item) {
   item.addEventListener('click', (item) => {
     let tab = item.currentTarget;
     let arrow = tab.querySelector('.options-arrow');
 
     let parent = tab.parentElement;
-      // if active clicked - close it
-    if(parent.classList.contains('active')){
+    // if active clicked - close it
+    if (parent.classList.contains('active')) {
       arrow.classList.remove('arrow-open');
       parent.classList.remove('active');
 
@@ -72,46 +72,46 @@ Array.from(optionsItems).forEach(function(item) {
     } else {
       removeClass('options-arrow', 'arrow-open');
       removeClass('options-box-mob__option', 'active');
-      
+
       arrow.classList.add('arrow-open');
       parent.classList.add('active');
-    }    
+    }
   });
 });
 
 var submitContactForm = () => {
   let url = 'https://api.backendless.com/6D0CC6DE-4FD2-C484-FFC4-F47290F29700/0116A92C-8F97-4098-8F0D-1DAD483D040C/data/MarketingCRM';
 
-  const theWholeName = document.getElementById('name').value
+  const name = document.getElementById('name').value
+  const nameParts = name.split(" ")
 
-  const firstName = theWholeName.substr(0,theWholeName.indexOf(' '))
-  const lastName = theWholeName.substr(theWholeName.indexOf(' ')+1)
+  axios.post(url, {
+    registrationSource: 'atendit contact us',
+    firstName: nameParts[0] ?? '',
+    lastName: nameParts[1] ?? '',
+    emailAddress: document.getElementById('email').value,
+    messageContents: document.getElementById('message').value,
+    ownerId: 'DB61EABA-C2BD-07B1-FFF8-7E2ADA93AB00'
+  },
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('success', response)
+      let contactFormEl = document.getElementById("contact-form");
+      contactFormEl.classList.add("form-invisible");
+      contactFormEl.reset();
+    })
+    .catch(e => console.log('error occured', e))
 
-  let ojb =  {
-      registrationSource : 'atendit contact us',
-      firstName : firstName,
-      lastName: lastName,
-      emailAddress : document.getElementById('email').value,
-      messageContents : document.getElementById('message').value
-    }
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-          console.log(xhr.status)
-          console.log(xhr.responseText)
-
-        }
-
-    xhr.send(JSON.stringify(ojb));
-  
 }
 
 
 // show-hide answer on the help page
 var qaItems = document.getElementsByClassName("qa-item");
-Array.from(qaItems).forEach(function(item) {
+Array.from(qaItems).forEach(function (item) {
   item.addEventListener('click', (item) => {
     item.currentTarget.classList.toggle('active')
     let arrow = item.currentTarget.firstChild
@@ -120,20 +120,20 @@ Array.from(qaItems).forEach(function(item) {
 });
 
 // adds arrows to help pages 
-Array.from(qaItems).forEach(function(item) {
-  var node = document.createElement("LI"); 
+Array.from(qaItems).forEach(function (item) {
+  var node = document.createElement("LI");
   node.classList.add('arrow')
-  item.prepend(node); 
+  item.prepend(node);
 });
 
 // Switch gif image on the Welcome page - DEPRECATED
 
-function closeForm(){
+function closeForm() {
   document.querySelector('.contact-form').classList.add('form-invisible');
   document.querySelector('#expand-form').classList.remove('form-invisible');
 }
 
-function showImg(evt, imgId){
+function showImg(evt, imgId) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tab-img");
   for (i = 0; i < tabcontent.length; i++) {
@@ -167,34 +167,34 @@ var frameIntervalMsDesk = 1;
 function runSlideDesk(dir, oldActive) {
   // container to move
   var deskContainer = document.getElementById("app-feature-section__content");
-  if(dir == 'l'){
+  if (dir == 'l') {
     var pos = 0;
     var endPos = -100;
     var delta = -1 * moveDeltaDesk;
   } else {
     var pos = -100;
     var endPos = 0;
-    var delta = moveDeltaDesk;    
+    var delta = moveDeltaDesk;
   }
-  
+
   var id = setInterval(frame, frameIntervalMsDesk);
-  function frame() {    
-      if (pos == endPos) {
-        clearInterval(id);
-        deskContainer.style.marginLeft = 0;
-        //remove active from old
-        Array.from(oldActive).forEach(function(element) {
-            element.classList.remove('active');  
-        });
-      } else {
-        pos += delta; 
-        deskContainer.style.marginLeft = pos + '%'; 
-      }
-  
+  function frame() {
+    if (pos == endPos) {
+      clearInterval(id);
+      deskContainer.style.marginLeft = 0;
+      //remove active from old
+      Array.from(oldActive).forEach(function (element) {
+        element.classList.remove('active');
+      });
+    } else {
+      pos += delta;
+      deskContainer.style.marginLeft = pos + '%';
+    }
+
   }
 }
 
-function getChildIndex(child){
+function getChildIndex(child) {
   var parent = child.parentNode;
   // The equivalent of parent.children.indexOf(child)
   let index = Array.prototype.indexOf.call(parent.children, child);
@@ -202,29 +202,29 @@ function getChildIndex(child){
   return index;
 }
 
-function changeInterractive(className){
+function changeInterractive(className) {
   // remove active to all tabs
   var tabs = document.querySelectorAll(".bottom-container.active");
   let oldActiveIndex = getChildIndex(tabs[0]);
 
-  Array.from(tabs).forEach(function(tab) {
+  Array.from(tabs).forEach(function (tab) {
     tab.classList.remove('active');
   });
   event.currentTarget.classList.add('active')
   let newIndex = getChildIndex(event.currentTarget);
 
   // if clicked the same item - exit the method
-  if(newIndex == oldActiveIndex) return;
+  if (newIndex == oldActiveIndex) return;
   // remove active to all
-  var oldActive = document.querySelectorAll('.feature-interractive.active');  
+  var oldActive = document.querySelectorAll('.feature-interractive.active');
 
   // set active to the clicked
   var activeElements = document.getElementsByClassName(className);
-  Array.from(activeElements).forEach(function(element) {
+  Array.from(activeElements).forEach(function (element) {
     element.classList.add('active');
   });
 
-  
+
 
   let dir = newIndex > oldActiveIndex ? 'l' : 'r';
 
@@ -237,54 +237,54 @@ var frameIntervalMs = 1;
 function runSlideMob(dir, oldActive) {
   // container to move
   var appFeatureContainer = document.getElementById("app-feature-sliding-part");
-  if(dir == 'l'){
+  if (dir == 'l') {
     var pos = 0;
     var endPos = -100;
     var delta = -1 * moveDeltaMob;
   } else {
     var pos = -100;
     var endPos = 0;
-    var delta = moveDeltaMob;    
+    var delta = moveDeltaMob;
   }
-  
+
   var id = setInterval(frame, frameIntervalMs);
-  function frame() {    
-      if (pos == endPos) {
-        clearInterval(id);
-        appFeatureContainer.style.marginLeft = 0;
-        //remove active from old
-        Array.from(oldActive).forEach(function(element) {
-            element.classList.remove('active');  
-        });
-      } else {
-        pos += delta; 
-        appFeatureContainer.style.marginLeft = pos + '%'; 
-      }
-  
+  function frame() {
+    if (pos == endPos) {
+      clearInterval(id);
+      appFeatureContainer.style.marginLeft = 0;
+      //remove active from old
+      Array.from(oldActive).forEach(function (element) {
+        element.classList.remove('active');
+      });
+    } else {
+      pos += delta;
+      appFeatureContainer.style.marginLeft = pos + '%';
+    }
+
   }
 }
 
-function changeInterractiveMob(className, dir){  
+function changeInterractiveMob(className, dir) {
 
   var tabs = document.querySelectorAll(".navigation-dot.active");
   let oldActiveIndex = getChildIndex(tabs[0]);
 
-  Array.from(tabs).forEach(function(tab) {
+  Array.from(tabs).forEach(function (tab) {
     tab.classList.remove('active');
   });
-  
-  
+
+
   // previously active elements
-  var oldActive = document.querySelectorAll('.feature-interractive-mob.active');  
+  var oldActive = document.querySelectorAll('.feature-interractive-mob.active');
 
   // if it was not a swipe action but a tab click 
-  if(dir == false) {
+  if (dir == false) {
     event.currentTarget.classList.add('active')
     let newIndex = getChildIndex(event.currentTarget);
 
     // if clicked the same item - exit the method
-    if(newIndex == oldActiveIndex) return;
-    
+    if (newIndex == oldActiveIndex) return;
+
     var realDir = newIndex > oldActiveIndex ? 'l' : 'r';
   } else {
     var realDir = dir
@@ -292,7 +292,7 @@ function changeInterractiveMob(className, dir){
 
   // set active to the clicked
   var activeElements = document.getElementsByClassName(className);
-  Array.from(activeElements).forEach(function(element) {    
+  Array.from(activeElements).forEach(function (element) {
     element.classList.add('active');
   });
 
@@ -301,7 +301,7 @@ function changeInterractiveMob(className, dir){
 
 /* SWIPE */
 
-function detectSwipe(el,func) {
+function detectSwipe(el, func) {
   swipe_det = new Object();
   swipe_det.sX = 0;
   swipe_det.sY = 0;
@@ -315,23 +315,23 @@ function detectSwipe(el,func) {
   ele = document.getElementById(el);
 
   // if element not found
-  if(null == ele) return
+  if (null == ele) return
 
-  ele.addEventListener('touchstart',function(e){
+  ele.addEventListener('touchstart', function (e) {
     var t = e.touches[0];
-    swipe_det.sX = t.screenX; 
+    swipe_det.sX = t.screenX;
     swipe_det.sY = t.screenY;
-  },false);
-  ele.addEventListener('touchmove',function(e){
+  }, false);
+  ele.addEventListener('touchmove', function (e) {
     // e.preventDefault();
     var t = e.touches[0];
-    swipe_det.eX = t.screenX; 
-    swipe_det.eY = t.screenY;    
-  },false);
-  ele.addEventListener('touchend',function(e){
+    swipe_det.eX = t.screenX;
+    swipe_det.eY = t.screenY;
+  }, false);
+  ele.addEventListener('touchend', function (e) {
     //horizontal detection
     if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
-      if(swipe_det.eX > swipe_det.sX) direc = "r";
+      if (swipe_det.eX > swipe_det.sX) direc = "r";
       else direc = "l";
     }
     //vertical detection
@@ -343,11 +343,11 @@ function detectSwipe(el,func) {
     // }
 
     if (direc != "") {
-      if(typeof func == 'function') func(el,direc);
+      if (typeof func == 'function') func(el, direc);
     }
     direc = "";
-    
-  },false);  
+
+  }, false);
 }
 
 
@@ -357,32 +357,32 @@ function finishSwipe(el, direction) {
 
   var elements = document.getElementsByClassName("feature-content-mob");
 
-    for (i = 0; i < elements.length; i++){
-      let element = elements[i]
-    if(element.classList.contains('active')){
-      for (i = 0; i < element.classList.length; i++){
+  for (i = 0; i < elements.length; i++) {
+    let element = elements[i]
+    if (element.classList.contains('active')) {
+      for (i = 0; i < element.classList.length; i++) {
         cls = element.classList[i]
-        if(cls.startsWith('feature-mob')){
+        if (cls.startsWith('feature-mob')) {
           var elNum = parseInt(cls.slice(-1));
           // if we swipe to the left, activate next slide
-          if(direction == 'l'){
-            if(elNum < maxV) {
+          if (direction == 'l') {
+            if (elNum < maxV) {
               elNum += 1;
             } else {
               return
             }
           }
           // if we swipe to the right, activate previouse slide
-          if(direction == 'r'){
-            if(elNum > minV) {
+          if (direction == 'r') {
+            if (elNum > minV) {
               elNum -= 1;
             } else {
               return
             }
-          }          
+          }
           var newClass = cls.slice(0, -1) + elNum
 
-          changeInterractiveMob(newClass, direction)         
+          changeInterractiveMob(newClass, direction)
         }
       }
       break;
@@ -395,7 +395,7 @@ detectSwipe('app-feature-sliding-part-box', finishSwipe);
 
 /* / SWIPE */
 
-window.onload = function() {
+window.onload = function () {
   if (!window.location.href.includes('/select/')) {
     document.querySelector('#header').style.display = 'block';
   }
